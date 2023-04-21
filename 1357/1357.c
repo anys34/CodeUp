f885ee0300cefce33e9013ba8c5ff1e22c97b69f
anys34 <anyeseong34@gmail.com>
2023-04-21 23:31:58 +0900

    ADD | 1357

--- 1357/1357.c
+++ 1357/1357.c
@@ -3,14 +3,14 @@
 int main(void){
     int n;
     scanf("%d", &n);
-    for (int i = 1; i <= n; i++){
-        if (i <= n){
-            for(int j = 1; j <= i; j++){
+    for (int i = 0; i < n*2-1; i++){
+        if (i < n){
+            for(int j = 0; j <= i; j++){
                 printf("*");
             }
         }
         else{
-            for(int j = n; j <= i+5; j--){
+            for(int j = i; j < n*2-1; j++){
                 printf("*");
             }
         }
