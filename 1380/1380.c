472dbaf095769ad762de087808f1ab72d6ff88cd
anys34 <anyeseong34@gmail.com>
2023-04-22 11:01:04 +0900

    ADD | 1380

---
+++ 1380/1380.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+    for(int i = 1; i <= 6; i++)
+        for(int j = 1; j <= 6; j++)
+            if (i+j == n) 
+                printf("%d %d\n", i, j);
+    return 0;
+}
