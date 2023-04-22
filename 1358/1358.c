a776761e258dfae1d62b08dcc5f653c91a1a6668
anys34 <anyeseong34@gmail.com>
2023-04-22 10:39:21 +0900

    ADD | 1361

--- 1358/1358.c
+++ 1358/1358.c
@@ -4,16 +4,7 @@ int main(void) {
     int n,cnt=1;
     scanf("%d", &n);
     int j = (n-1)/2;
-    // for(int i=0;i<n;i++){
-    //     for(int j=n-1;i<j;j--){
-    //         printf(" ");
-    //     }
-    //     for(int k=0;k<2*i-1;k++){
-    //         printf("*");
-    //     }
-    //     printf("\n");
-    // }
-
+    
     while(cnt != n+2) {
         for(int i = 0; i < j; i++) {
             printf(" ");
