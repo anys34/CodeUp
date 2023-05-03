541381c3bbbf4497517b39a94f0b09a5d7f14e67
anys34 <anyeseong34@gmail.com>
2023-05-03 11:34:01 +0900

    ADD | 1127

--- 1126/1126.c
+++ 1126/1126.c
@@ -3,10 +3,10 @@
 int main(void) {
     int a,b;
     scanf("%d %d", &a, &b);
-    printf("%d + %d = %d", a, b, a+b);
-    printf("%d - %d = %d", a, b, a-b);
-    printf("%d * %d = %d", a, b, a*b);
-    printf("%d / %d = %d", a, b, a/b);
-    printf("%d %% %d = %d", a, b, a%b);
+    printf("%d + %d = %d\n", a, b, a+b);
+    printf("%d - %d = %d\n", a, b, a-b);
+    printf("%d * %d = %d\n", a, b, a*b);
+    printf("%d / %d = %d\n", a, b, a/b);
+    printf("%d %% %d = %d\n", a, b, a%b);
     return 0;
 }
