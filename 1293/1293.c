cb94fa9e3583ece0381476b35df4c29160c6bc66
anys34 <anyeseong34@gmail.com>
2023-05-05 22:16:26 +0900

    ADD | 1293

---
+++ 1293/1293.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+    int n, max = 0, min = 100;
+    scanf("%d", &n);
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+        if (a[i] > max)
+            max = a[i];
+        if (a[i] < min)
+            min = a[i];
+    }
+    printf("%d %d", max, min);
+    return 0;
+}
