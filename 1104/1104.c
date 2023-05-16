63f96f4f7d9e07348429e3fbb6d9e042b69e63fc
anys34 <anyeseong34@gmail.com>
2023-05-16 22:30:25 +0900

    ADD | 1104

---
+++ 1104/1104.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("nayoung12선생님 감사합니다.\n");
+    printf("C언어 수업 너무 재미있어요\n");
+    printf("재미있는 수업 계속 해주세요\n");
+    printf("항상 행복하세요~");
+    return 0;
+}
