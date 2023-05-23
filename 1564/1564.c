dbf0f1ea4ba1b68868696df8df9255f44acbc8f7
anys34 <anyeseong34@gmail.com>
2023-05-23 10:38:29 +0900

    ADD | 1564

---
+++ 1564/1564.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int a, b;
+
+int gcd(int a, int b) {
+    for(int i = a+b; i >= 0; i--) {
+        if (a % i == 0 && b % i == 0)
+            return i;
+    }
+}
+
+int main()
+{
+  scanf("%d%d", &a, &b);
+  printf("%d\n", gcd(a, b));
+}
