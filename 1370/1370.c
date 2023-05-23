7f8bbb7ac5c73fee7174f1b7f3ec6a8ffafe15bb
anys34 <anyeseong34@gmail.com>
2023-05-23 16:51:13 +0900

    ADD | 1370

---
+++ 1370/1370.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+    int h, r;
+    scanf("%d %d", &h, &r);
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < h; j++) {
+            for (int k = 0; k < j; k++)
+                printf(" ");
+            printf("*\n");
+        }
+       for (int j = h-2; j >= 0; j--) {
+            for (int k = 0; k < j; k++)
+                printf(" ");
+            printf("*\n");
+       }
+    }
+    return 0;
+}
