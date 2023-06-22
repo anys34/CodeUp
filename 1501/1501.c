4b5a008950c4950d6ff58bc7e5ef84cb54c2f94a
anys34 <anyeseong34@gmail.com>
2023-06-22 22:33:58 +0000

    ADD | 1501

---
+++ 1501/1501.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+    int n, num=1;
+    scanf("%d", &n);
+    int a[n][n];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            a[i][j] = num;
+            num++;
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
