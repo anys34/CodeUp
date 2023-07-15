6e472e75677e5b5380aa2a528b97721442b3bed6
anys34 <anyeseong34@gmail.com>
2023-07-15 23:54:07 +0900

    ADD | 1541

---
+++ 1541/1541.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int n;
+
+void f(int n) {
+    if (n == 0) printf("zero");
+    else if (n < 0) printf("negative");
+    else printf("positive");
+}
+
+int main()
+{
+  scanf("%d", &n);
+  f(n);
+  return 0;
+}
