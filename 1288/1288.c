144831102c82f2870b55e03e1b73c18f10c5d6c4
anys34 <anyeseong34@gmail.com>
2023-07-31 22:23:24 +0000

    FIX | 1288

--- 1288/1288.c
+++ 1288/1288.c
@@ -1,14 +1,16 @@
-#include <stdio.h>
+# include <stdio.h>
 
 int main(void) {
-    int n, cnt=0;
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cnt++;
-            printf("%d ", cnt);
-        }
-        printf("\n");
-    }
-    return 0;
+	int n,r,m,i;
+	int n_sum=1,r_sum=1,m_sum=1;
+	scanf("%d %d",&n,&r);
+	m = n-r;
+	for(i = n; i>=1; i--)
+		n_sum = n_sum *i;
+	for(i = r; i>=1; i--)
+		r_sum = r_sum * i;
+	for(i = m; i>=1; i--)
+		m_sum = m_sum * i;
+	printf("%d",n_sum/(r_sum*m_sum));
+	return 0;
 }
