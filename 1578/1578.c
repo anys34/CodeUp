e361f84618d31f5178132206c9fa6a06cfa61070
anys34 <anyeseong34@gmail.com>
2023-08-16 20:32:13 +0900

    ADD | 1578

---
+++ 1578/1578.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int mymax(int a, int b) {
+    if (a > b) return a;
+    return b;
+}
+
+int main(void) {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", mymax(a, b));
+    return 0;
+}
