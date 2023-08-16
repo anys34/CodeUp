93b4e23becd2054ab61cdc8c0f40b40379f1144b
anys34 <anyeseong34@gmail.com>
2023-08-17 08:28:01 +0900

    ADD | 1553

---
+++ 1553/1553.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+double x;
+
+long long int f(double x) {
+    long long int temp = (long long int)x;
+    if (temp < x) return temp + 1;
+    return temp;
+}
+
+int main(void) {
+  scanf("%lf", &x);
+  printf("%lld\n", f(x));
+}
