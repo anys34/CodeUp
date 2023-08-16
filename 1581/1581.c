e632e8955375df2e3e6b8536ee39b3abff449712
anys34 <anyeseong34@gmail.com>
2023-08-16 22:45:32 +0900

    ADD | 1581

---
+++ 1581/1581.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void myswap(int* a, int* b) {
+    if (*a > *b) {
+        int temp = *a;
+        *a = *b;
+        *b = temp;
+    }
+}
+
+int main(void) {
+    int a, b;
+    scanf("%d%d", &a, &b);
+    myswap(&a, &b);
+    printf("%d %d", a, b);
+    return 0;
+}
