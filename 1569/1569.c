6eb69c4a25a51ecb6c5243d95528d492c74e1894
anys34 <anyeseong34@gmail.com>
2023-09-07 23:11:39 +0000

    ADD :: 1569

---
+++ 1569/1569.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int n, k, d[1010];
+
+int findi(int k) {
+    for (int i = 1; i <= n; i++)
+        if (k == d[i]) return i;
+    return -1;
+}
+
+int main()
+{
+  scanf("%d", &n);
+
+  for(int i=1; i<=n; i++)
+    scanf("%d", &d[i]);
+
+  scanf("%d", &k);
+
+  printf("%d\n", findi(k));
+}
